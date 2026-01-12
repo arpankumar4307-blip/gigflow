56d188817f4d802a971230f7dfbdbb1a282b5ea0
arpankumar4307-blip <arpankumar4307@gmail.com>
2026-01-13 01:31:45 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-  name:String,
-  email:{type:String, unique:true},
-  password:String
+  name: String,
+  email: { type: String, unique: true },
+  password: String
 })
 
 module.exports = mongoose.model("User", userSchema)
-
