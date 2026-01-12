4c53b81237db085fd674f8055f855d33722dad0b
arpankumar4307-blip <arpankumar4307@gmail.com>
2026-01-13 01:33:42 +0530

    Update gigs.js

--- backend/ routes/gigs.js
+++ backend/ routes/gigs.js
@@ -1,1 +1,28 @@
+const router = require("express").Router()
+const Gig = require("../models/Gig")
+const auth = require("../middleware/auth")
+
+router.get("/", async (req, res) => {
+  const search = req.query.search
+
+  const gigs = search
+    ? await Gig.find({
+        title: { $regex: search, $options: "i" },
+        status: "open"
+      })
+    : await Gig.find({ status: "open" })
+
+  res.json(gigs)
+})
+
+router.post("/", auth, async (req, res) => {
+  const gig = await Gig.create({
+    ...req.body,
+    ownerId: req.user.id
+  })
+
+  res.json(gig)
+})
+
+module.exports = router
 
